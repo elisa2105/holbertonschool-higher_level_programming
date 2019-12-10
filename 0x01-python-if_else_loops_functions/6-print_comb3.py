df901d421aa0a42fb1b0c6e753964eb749374d1c
elisa2105 <aikomi1726@gmail.com>
2019-12-10 18:34:47 +0000

    operators fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-for i in range(0,10):
-    for j in range(0,10):
-        if i<j:
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i < j:
             print("{}{}".format(i, j), end="")
-            if i == 8 and j==9:
+            if i == 8 and j == 9:
                 continue
             else:
                 print(", ", end="")
