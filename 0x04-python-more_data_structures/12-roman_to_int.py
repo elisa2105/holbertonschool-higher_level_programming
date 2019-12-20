5b58807dc84233490d2d4733524d6862cac9e308
Elisa2105 <53063647+elisa2105@users.noreply.github.com>
2019-12-19 20:38:20 -0500

    converts a Roman numeral to an integer fixed loop

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if roman_string and type(roman_string) == str:
-        i = roman_string
-        j = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        res = 0
-        for k in range(len(i)):
-            for l, m in j.items():
-                if k == i[k]:
-                    if k + 1 < len(i) and j[i[k + 1]] > j[i[k]]:
-                        res -= m
+        sh = 0
+        a = roman_string
+        b = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        for j in range(len(a)):
+            for i, k in b.items():
+                if i == a[j]:
+                    if j + 1 < len(a) and b[a[j + 1]] > b[a[j]]:
+                        sh -= k
                     else:
-                        res += m
-        return res
+                        sh += k
+        return sh
     return 0
