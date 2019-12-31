547bd792151a929bfeb09f4284e93ce5147de063
elisa2105 <aikomi1726@gmail.com>
2019-12-31 21:40:35 +0000

    retrieves an element from a list like in C

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+        if idx > len(my_list) - 1:
+            return None
+        elif idx < 0:
+            return None
+        else:
+            for i in range(len(my_list)):
+                    if i == idx:
+                        return my_list[i]
+                    else:
+                        continue
