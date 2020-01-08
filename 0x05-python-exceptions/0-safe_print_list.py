ab83ad589283d99a257f943a8eb781cc8fe40442
elisa2105 <aikomi1726@gmail.com>
2020-01-08 17:54:44 +0000

    pep8 fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         i = 0
         while i < x:
             print('{}'.format(my_list[i]), end="")
-            i += 1 
+            i += 1
         print("")
         return i
     except IndexError:
