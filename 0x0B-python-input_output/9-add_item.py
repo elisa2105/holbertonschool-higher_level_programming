2f7f3b5ec6e5800c11f7e0d0d7f08c9f13b23a86
elisa2105 <aikomi1726@gmail.com>
2020-01-22 01:38:10 +0000

    fixed calls

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -6,12 +6,12 @@ import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-nlist = sys.argv[1:]
+new_list = sys.argv[1:]
 
 try:
-    olist = load_from_json_file("add_item.json")
+    old_list = load_from_json_file("add_item.json")
 except:
-    olist = []
+    old_list = []
 
-olist.extend(nlist)
-save_to_json_file(olist, "add_item.json")
+old_list.extend(new_list)
+save_to_json_file(old_list, "add_item.json")
