4fdebc5a9777a619897444612d22e23d52366111
elisa2105 <aikomi1726@gmail.com>
2020-01-22 01:07:05 +0000

    fixed imports

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
+"""save arguments"""
+
+
 import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+import json
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
 
-def add_item(arguments, filename):
-    '''
-    Adds arguments to a list and save them into JSON file
-    [:param arguments:] the arguments
-    [:param filename:] the file
-    '''
-    try:
-        save = load_from_json_file(filename)
-    except:
-        save = []
-
-    for i in arguments:
-        save.append(i)
-    save_to_json_file(save, filename)
-
-if __name__ == "__main__":
-    arguments = sys.argv[1:]
-    filename = "add_item.json"
-    add_item(arguments, filename)
+args =  sys.argv[1:]
+v = None
+try:
+    v = load_from_json_file("add_item.json")
+except IOError:
+    v = []
+v = v + args
+print(v)
+save_to_json_file(v, "add_item.json")
