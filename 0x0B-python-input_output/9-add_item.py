d189f785b6fa245fdfc9746f740e8d1ed750cb55
elisa2105 <aikomi1726@gmail.com>
2020-01-22 01:12:26 +0000

    fixed imports

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""save arguments"""
-
-
 import sys
-import json
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 
-args =  sys.argv[1:]
-v = None
-try:
-    v = load_from_json_file("add_item.json")
-except IOError:
-    v = []
-v = v + args
-print(v)
-save_to_json_file(v, "add_item.json")
+def add_item(arguments, filename):
+    '''
+    Adds arguments to a Python list and then saves them to a JSON file
+    [:param arguments:] the arguments to be added
+    [:param filename:] the name of the file to save to
+    '''
+    try:
+        to_save = load_from_json_file(filename)
+    except:
+        to_save = []
+
+    for arg in arguments:
+        to_save.append(arg)
+    save_to_json_file(to_save, filename)
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    filename = "add_item.json"
+    add_item(arguments, filename)
