c915c3dc66c5661233fa2947c72316a988070894
elisa2105 <aikomi1726@gmail.com>
2020-01-22 00:53:27 +0000

    fixed filet8 error

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -7,5 +7,5 @@ def load_from_json_file(filename):
     Creates an object from a JSON file
     '''
     with open(filename, encoding='utf-8') as filet8:
-        obj = json.load(f)
+        obj = json.load(filet8)
     return obj
