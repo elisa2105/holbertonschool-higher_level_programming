8c09bd963aafc34fd9446418b662f28faa09da11
elisa2105 <aikomi1726@gmail.com>
2020-04-15 22:58:38 +0000

    manage HTTP error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""manage HTTPError"""
+import urllib.error
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            print((response.read()).decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
