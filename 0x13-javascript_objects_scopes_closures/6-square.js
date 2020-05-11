d9b2961d3c6ed89246583168373fcc99ed9fdea0
elisa2105 <aikomi1726@gmail.com>
2020-05-11 23:48:35 +0000

    fixed print

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,7 +5,7 @@ const Square = class extends SquareG {
   charPrint (c) {
     if (c !== undefined) {
       for (let i = 0; i < this.height; i++) {
-        console.log('C'.repeat(this.width));
+        console.log(c.repeat(this.width));
       }
     } else {
       this.print();
