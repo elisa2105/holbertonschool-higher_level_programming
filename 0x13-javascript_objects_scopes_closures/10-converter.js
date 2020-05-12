97685207ce1f2581fbd2f0aa7a0fc999f618f4d9
elisa2105 <aikomi1726@gmail.com>
2020-05-12 02:47:09 +0000

    fixed function

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-exports.converter = function nombre (base) {
-  // return(number => number.toString(base));
-  function conv (num) {
-    const result = num.toString(base);
-    return result;
-  }
-  return (conv);
+exports.converter = function (base) {
+  return number => number.toString(base);
 };
